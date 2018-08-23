9c517d6630c70860d09c6b82a8e4c0f87ae04529
Viswa Sunny <viswasunny007@gmail.com>
2018-08-23 10:49:13 +0530

    Create 95.java

---
+++ 95.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int p=s.nextInt();
+		int t=s.nextInt();
+		int r=s.nextInt();
+		System.out.print((p*t*r)/100);
+	}
+}
