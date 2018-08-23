22d90722bdda494ba45a9fb245e9810b027f1516
Viswa Sunny <viswasunny007@gmail.com>
2018-08-23 10:32:06 +0530

    Create 93.java

---
+++ 93.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		System.out.print(n+273);
+	}
+}
